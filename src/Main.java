f292b13d86a80e6e4e6ee0a350fb34e56dc1038e
ebraun99 <ericbraun@outlook.com>
2017-11-08 10:42:16 -0600

    changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,20 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int[] score = new int[]{
-        };
+        int currentTime = 0;
+        int[] arrivalTime = {3,3,9};
+        int[] serviceTime = {2,15,14};
+        int currentMax = -1;
+        int wait = 0;
+
+
+        for (int i=0;i<serviceTime.length;i++)
+        {
+            wait = Math.max(currentTime, arrivalTime[i]);
+            currentMax = Math.max(currentMax, wait - arrivalTime[i]);
+            currentTime = wait + serviceTime[i];
+        }
+
+        System.out.println("Current Max is: " + currentMax);
     }
 }
