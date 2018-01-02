6485995e940719e3a0250fbfa002b9bc0bb69871
ebraun99 <ericbraun@outlook.com>
2018-01-02 09:34:05 -0600

    new

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,99 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main
 {
-    public static String string = "5; 1,3,4,5,1";
-    public static int currentValue;
-    public static HashSet<Integer> set = new HashSet<Integer>();
-    public static String newString;
+    private static String input = "0,1,2,3,4;5";
+    private static int semicolon;
+    private static int sum;
+    private static ArrayList<String> arrayList;
 
     public static void main(String[] args)
     {
-        replaceSpecialCharacters();
-        getCurrentValue();
-    }
+        //find the index of semicolon
+        semicolon = input.indexOf(";");
 
-    public static String replaceSpecialCharacters()
-    {
-        newString = string.replace(",","");
-        newString = newString.replace(";","");
-        newString = newString.replace(" ","");
-        newString = newString.substring(1);
+        //create int with the sum
+        sum = Integer.parseInt(input.substring(semicolon + 1,semicolon+2));
 
-        return newString;
-    }
+        //remove commas
+        input = input.substring(0,semicolon);
+        input = input.replace(",","");
 
-    public static void getCurrentValue()
-    {
-        for (int i = 0; i <newString.length() ; i++)
+        //change into int array
+        char[] charArray = input.toCharArray();
+
+        int[] num1intarray = new int[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++){
+            num1intarray[i] = Character.getNumericValue(charArray[i]);
+        }
+
+
+
+
+        for (int i = 0; i <num1intarray.length; i++)
         {
-            currentValue = Character.getNumericValue(newString.charAt(i));
-
-            if(checkDuplicate())
-            {
-                System.out.println(currentValue);
+            for (int j = i + 1; j <num1intarray.length; j++) {
+                if(num1intarray[i] + num1intarray[j] == sum)
+                {
+                    arrayList.add(num1intarray[i] + "+" + num1intarray[j]);
+                }
             }
         }
+
+        System.out.println(arrayList);
+
+
+
     }
 
-    public static boolean checkDuplicate(){
-        if(!set.add(currentValue))
-        {
-            return true;
-        }
 
-        else
-            return false;
-    }
+
+
+//    public static String string = "5; 1,3,4,5,1";
+//    public static int currentValue;
+//    public static HashSet<Integer> set = new HashSet<Integer>();
+//    public static String newString;
+//
+//    public static void main(String[] args)
+//    {
+//        replaceSpecialCharacters();
+//        getCurrentValue();
+//    }
+//
+//    public static String replaceSpecialCharacters()
+//    {
+//        newString = string.replace(",","");
+//        newString = newString.replace(";","");
+//        newString = newString.replace(" ","");
+//        newString = newString.substring(1);
+//
+//        return newString;
+//    }
+//
+//    public static void getCurrentValue()
+//    {
+//        for (int i = 0; i <newString.length() ; i++)
+//        {
+//            currentValue = Character.getNumericValue(newString.charAt(i));
+//
+//            if(checkDuplicate())
+//            {
+//                System.out.println(currentValue);
+//            }
+//        }
+//    }
+//
+//    public static boolean checkDuplicate(){
+//        if(!set.add(currentValue))
+//        {
+//            return true;
+//        }
+//
+//        else
+//            return false;
+//    }
 }
